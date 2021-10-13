7097ec8f5c432da4f98545a143f9cc14d827b41d
Linfeng Qian <thewawar@gmail.com>
2021-10-13 14:26:32 +0800

    First Commit

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![feature(asm)]
+
+#[no_mangle]
+pub fn foo(a: u32) -> u32 {
+    let b = if a > 3 {
+        333
+    } else if a == 3 {
+        33
+    } else {
+        3
+    };
+    b
+}
+
+#[no_mangle]
+pub fn foo2(mut a: u32) -> u32 {
+    /*
+    sext.w  a1, a0
+    addi    a3, zero, 3
+    addi    a2, zero, 33
+    bne     a1, a3, .LBB0_3
+    addi    a0, zero, 333
+    bgeu    a3, a1, .LBB0_4
+    .LBB0_2:
+    ret
+    .LBB0_3:
+    addi    a2, zero, 3
+    addi    a0, zero, 333
+    bltu    a3, a1, .LBB0_2
+    .LBB0_4:
+    mv      a0, a2
+    ret
+    */
+    unsafe {
+        asm!(
+            "sext.w  a1, a0",
+            "addi    a3, zero, 3",
+            "addi    a2, zero, 33",
+            "bne     a1, a3, 3f",
+            "addi    a0, zero, 333",
+            "bgeu    a3, a1, 4f",
+            "2:",
+            "ret",
+            "3:" ,
+            "addi    a2, zero, 3",
+            "addi    a0, zero, 333",
+            "bltu    a3, a1, 2b",
+            "4:",
+            "mv      a0, a2",
+            inout("a0") a,
+        )
+    }
+    a
+}
